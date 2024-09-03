553ffe1e4dcc7b1a0ce1ecf0b33ab5bb15a396d0
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-09-03 16:07:01 -0300

    feat: Iniciando projeto

---
+++ js/conectarApi.js
@@ -0,0 +1,28 @@
+async function listarVideos()
+{
+    const conexao = await fetch("http://localhost:3000/videos");
+    return await conexao.json();
+}
+
+async function criarVideo(titulo, descricao, url, imagem)
+{
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem,
+        }),
+    });
+
+    return await conexao.json();
+}
+
+export const conectarApi = {
+    listarVideos,
+    criarVideo,
+}
---
+++ js/mostrarVideos.js
@@ -0,0 +1,32 @@
+import { conectarApi } from "./conectarApi.js";
+
+const lista = document.querySelector("[data-lista]");
+
+function construirCard(titulo, descricao, url, imagem)
+{
+    const video = document.createElement("li");
+    video.className = "videos__item";
+    video.innerHTML = `
+        <iframe width="100%" height="72%" src="${imagem}"
+            title="${titulo}" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        <div class="descricao-video">
+            <img src="${imagem}" alt="logo canal alura">
+            <h3>${titulo}</h3>
+            <p>${descricao}</p>
+        </div>
+    `;
+
+    return video;
+}
+
+async function listarVideos()
+{
+    const listaApi = await conectarApi.listarVideos();
+    listaApi.forEach(element => 
+        lista.appendChild(construirCard(element.titulo, element.descricao, element.url, element.imagem))
+    );
+}
+
+listarVideos();
